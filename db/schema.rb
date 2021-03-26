b5aaa267ed6974c354bf219c0aa7e6c9f6274b69
jfrey212 <eojyerf@gmail.com>
2021-03-25 22:05:51 -0500

    Article scaffold and database migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_032315) do
+ActiveRecord::Schema.define(version: 2021_03_08_034501) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_03_08_032315) do
     t.datetime "published_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "excerpt"
+    t.string "location"
   end
 
 end
