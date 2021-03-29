cc031e7690fffdc3c39ed71d648ad572f0ad07d0
jfrey212 <eojyerf@gmail.com>
2021-03-29 00:58:54 -0500

    Created and ran migrations for categories table and articles categories join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_051037) do
+ActiveRecord::Schema.define(version: 2021_03_29_055516) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(version: 2021_03_28_051037) do
     t.string "location"
     t.integer "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "articles_categories", id: false, force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.integer "category_id", null: false
+    t.index ["article_id", "category_id"], name: "index_articles_categories_on_article_id_and_category_id"
+    t.index ["category_id", "article_id"], name: "index_articles_categories_on_category_id_and_article_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "profiles", force: :cascade do |t|
